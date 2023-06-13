f627d7642e54c055d38da8913a8a29b0a08ea1f2
hoailam066 <hoailam066@gmail.com>
2023-06-13 21:21:34 +0700

    a

---
+++ TestTCPIP/TestTCPIP/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestTCPIP.Models;
+
+namespace TestTCPIP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string ip, string port, string message)
+        {
+            if (ip != null && port != null && message != null)
+            {
+                new TCPIPModel().SendMessage(ip, int.Parse(port), message);
+                ViewData["message"] = message;
+                ViewData["ip"] = ip;
+                ViewData["port"] = port;
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestTCPIP/TestTCPIP/Models/TCPIPModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TestTCPIP.Models
+{
+    public class TCPIPModel
+    {
+        public TCPIPModel() { }
+        public bool SendMessage(string ipremote, int port, string message)
+        {
+            TcpClient m_client = new TcpClient();
+            m_client.SendTimeout = 3000;
+            m_client.ReceiveTimeout = 3000;
+            m_client.Connect(ipremote, port);
+            System.Threading.Thread.Sleep(200);
+            if (!m_client.Connected)
+            {
+                return false;
+            }
+            else
+            {
+                byte[] buff = System.Text.Encoding.Default.GetBytes(message.ToCharArray());
+                m_client.GetStream().Write(buff, 0, buff.Length);
+                return true;
+            }    
+        }
+    }
+}
